c3344e745559d9107568af1d49cc8b2f6a92de34
zoltan-gacsal <zoltan.gacsal87@gmail.com>
2024-05-03 17:26:02 +0200

    header with Navbar

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
